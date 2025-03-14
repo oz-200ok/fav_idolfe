2d64d20e21c9c56257d42d98a38608c226365ca9
yeowun05-12 <taking0n@naver.com>
2025-03-14 17:52:04 +0900

    [init] 폴더구조 기초세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import '@toast-ui/calendar/dist/toastui-calendar.min.css';
 import React from 'react';
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>dfd</div>
+    </>
+  );
 }
 
 export default App;
