b53bf7d37d6c45475b1c5b94e7b2278870358d0f
ddumni0227 <ddum_ni0227@kakao.com>
2025-03-18 18:27:17 +0900

    ✨ [feat]Header-공통컴포넌트 개발중

--- src/component/Header/index.tsx
+++ src/component/Header/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import style from './Header.module.scss';
+import './Header.scss';
 import logo from '../../assets/9.png';
 import searchIcon from '../../assets/search.png';
 import logoutIcon from '../../assets/logout.png';
@@ -8,7 +8,8 @@ import logoutIcon from '../../assets/logout.png';
 //헤더 컴포넌트 정의
 function Header() {
   //현재 로그인 상태를 저장할 state
-  const [userRole, setUserRole] = useState('guest');
+  //유저상태 전역으로 관리
+  const [userRole, setUserRole] = useState('user');
   // 기본값: "guest" = 로그인 전
   // "user" = 일반 사용자 로그인 후
   // "admin" = 관리자 로그인 후
@@ -25,72 +26,72 @@ function Header() {
   };
 
   return (
-    <header className={style.header}>
+    <header className="header">
       {/* 로고 */}
-      <div className={style.logo}>
+      <div className="logo">
         <Link to="/">
           <img src={logo} alt="I log" />
         </Link>
       </div>
       {/* 네비게이션 메뉴 */}
-      <nav className={style.nav}>
+      <nav className="nav">
         {userRole === 'guest' && (
           //로그인 전
-          <div className={style.navLinks}>
+          <div className="nav_Links">
             <Link to="/login">
-              <button>로그인</button>
+              <button className="login_Button">로그인</button>
             </Link>
             <Link to="/signup">
-              <button>회원가입</button>
+              <button className="signup_Button">회원가입</button>
             </Link>
           </div>
         )}
         {/* 일반 사용자 */}
         {userRole === 'user' && (
-          <div className={style.navLinks}>
+          <div className="nav_Links">
             {/* 검색창 */}
-            <img src={searchIcon} alt="search" className={style.searchIcon} />
-            <input
-              type="text"
-              value={searchQuery}
-              onChange={handleSearchChange}
-              placeholder="아티스트 검색"
-            ></input>
+            <div className="search">
+              <img src={searchIcon} alt="search" className="search_Icon" />
+              <input
+                type="text"
+                value={searchQuery}
+                onChange={handleSearchChange}
+                placeholder="아티스트 검색"
+              ></input>
+            </div>
+
             {/* 버튼들 */}
             <Link to="/mypage">
-              <button>마이페이지</button>
+              <button className="page_Button">마이페이지</button>
             </Link>
-            <button onClick={handleLogout}>
-              <img
-                src={logoutIcon}
-                alt="로그아웃"
-                className={style.logoutIcon}
-              />
+            <button onClick={handleLogout} className="logout_Button">
+              <img src={logoutIcon} alt="로그아웃" className="logout_Icon" />
             </button>
           </div>
         )}
         {/* 관리자 */}
         {userRole === 'admin' && (
-          <div className={style.navLinks}>
+          <div className="nav_Links">
             {/* 검색창 */}
-            <img src={searchIcon} alt="search" className={style.searchIcon} />
-            <input
-              type="text"
-              value={searchQuery}
-              onChange={handleSearchChange}
-              placeholder="아티스트 검색"
-            ></input>
+            <div className="search">
+              <img src={searchIcon} alt="search" className="search_Icon" />
+              <input
+                type="text"
+                value={searchQuery}
+                onChange={handleSearchChange}
+                placeholder="아티스트 검색"
+              ></input>
+            </div>
+
             {/* 버튼들 */}
-            <Link to="/add">일정추가</Link>
-            <Link to="/admin">
-              <button>관리자페이지</button>
+            <Link to="/add">
+              <button className="add_Button">일정추가</button>
             </Link>
-            <button onClick={handleLogout}>
-              <img
-                src={logoutIcon}
-                alt="로그아웃"
-                className={style.logoutIcon}
-              />
+            <Link to="/admin">
+              <button className="page_Button">관리자페이지</button>
+            </Link>
+            <button onClick={handleLogout} className="logout_Button">
+              <img src={logoutIcon} alt="로그아웃" className="logout_Icon" />
             </button>
           </div>
         )}
