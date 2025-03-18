10f9352f23d9ac8ffbd0d1923756e95fef2d42ff
sujeong9928 <tneoddl9928@naver.com>
2025-03-18 18:29:08 +0900

    ✨ feat: 사이드바 UI 및 기능 추가

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -1,44 +1,50 @@
 
+import React, { useState } from "react";
 import "./Sidebar.scss";
 
 interface SidebarProps {
-  userType: "user" | "admin"; // "user" = 일반 사용자, "admin" = 관리자
+  userType: "user" | "admin"; // 일반 사용자 or 관리자
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ userType }) => {
   const isAdmin = userType === "admin";
 
+  // 구독 목록 & 그룹 목록 접기 기능 상태
+  const [isListOpen, setIsListOpen] = useState(true);
+
   return (
-    
     <aside className="sidebar">
       {/* 상단 헤더 */}
-      <p>슬픔이</p>
-      <div className="sidebar__header">
+      <div className="sidebar_header">
         <span>{isAdmin ? "그룹 관리" : "최애 캘린더"}</span>
-        <button className="sidebar__add">+</button>
+        <button className="sidebar_toggle" onClick={() => setIsListOpen(!isListOpen)}>
+          {isListOpen ? "ˆ" : "ˇ"}
+        </button>
       </div>
 
-      {/* 구독한 아이돌 / 관리 그룹 목록 */}
-      <ul className="sidebar__list">
-        {isAdmin ? (
-          <>
-            <li className="sidebar__item">
-              그룹 1 <button className="sidebar__edit">그룹 정보 수정</button>
-            </li>
-            <li className="sidebar__item">
-              그룹 2 <button className="sidebar__edit">그룹 정보 수정</button>
-            </li>
-          </>
-        ) : (
-          <>
-            <li className="sidebar__item">구독한 아이돌 1</li>
-            <li className="sidebar__item">구독한 아이돌 2</li>
-          </>
-        )}
-      </ul>
+      {/* 구독한 아이돌 / 관리 그룹 목록 (접기 기능 적용) */}
+      {isListOpen && (
+        <ul className="sidebar_list">
+          {isAdmin ? (
+            <>
+              <li className="sidebar_item">
+                그룹 1 <button className="sidebar_edit">그룹 정보 수정</button>
+              </li>
+              <li className="sidebar_item">
+                그룹 2 <button className="sidebar_edit">그룹 정보 수정</button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="sidebar_item">구독한 아이돌 1</li>
+              <li className="sidebar_item">구독한 아이돌 2</li>
+            </>
+          )}
+        </ul>
+      )}
 
-      {/* 관리 그룹 보기 버튼 */}
-      {isAdmin && <button className="sidebar__view">관리 그룹 보기</button>}
+      {/* 관리 그룹 보기 버튼 (위아래 여백 수정) */}
+      {isAdmin && <button className="sidebar_view">관리 그룹 보기</button>}
     </aside>
   );
 };
