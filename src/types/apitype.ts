411e9d73cc35a735fd7e10af9bba80f2ca5371cc
yeowun05-12 <taking0n@naver.com>
2025-03-18 11:14:16 +0900

    ✨ [feat] 회원가입 기능 브랜치 생성
    회원가입 기능 브랜치 생성

--- src/types/apitype.ts
+++ src/types/apitype.ts
@@ -1,17 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 
 // POST 로그인 기능 API 타입 선언
 type Loginstype = {
-    'access_token': string;
-    'refresh_token': string;
-    'expires_in': number;
-    'user': {
-        'id': number;
-        'email': string;
-    }
-}
+  access_token: string;
+  refresh_token: string;
+  expires_in: number;
+  user: {
+    id: number;
+    email: string;
+  };
+};
 
-export function hi () {
-    const respone = axios.get<Loginstype>("링크")
-    console.log(respone)
+export function hi() {
+  const respone1 = axios.get<Loginstype>('링크');
+  console.log(respone1);
 }
