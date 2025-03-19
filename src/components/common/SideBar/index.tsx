15a0c1c5c66eac0d738d82e00358b05336448a42
yeowun05-12 <taking0n@naver.com>
2025-03-19 18:02:00 +0900

    🎨[Design] 사이드바 추가

---
+++ src/components/common/SideBar/index.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './Sidebar.scss';
+
+interface SidebarProps {
+  userType: 'guest' | 'user' | 'admin'; // 일반 사용자 or 관리자
+}
+
+const SideBar: React.FC<SidebarProps> = ({ userType }) => {
+  const isAdmin = userType === 'admin';
+  const [isListOpen, setIsListOpen] = useState(true);
+
+  return (
+    <aside className="sidebar">
+      <div className="sidebar_header">
+        <span>{isAdmin ? '그룹 관리' : '최애 캘린더'}</span>
+        <button
+          className="sidebar_toggle"
+          onClick={() => setIsListOpen(!isListOpen)}
+        >
+          {isListOpen ? 'ˆ' : 'ˇ'}
+        </button>
+      </div>
+
+      {isListOpen && (
+        <ul className="sidebar_list">
+          {isAdmin ? (
+            <>
+              <li className="sidebar_item">
+                그룹 1 <button className="sidebar_edit">그룹 정보 수정</button>
+              </li>
+              <li className="sidebar_item">
+                그룹 2 <button className="sidebar_edit">그룹 정보 수정</button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="sidebar_item">구독한 아이돌 1</li>
+              <li className="sidebar_item">구독한 아이돌 2</li>
+            </>
+          )}
+        </ul>
+      )}
+
+      {isAdmin && <button className="sidebar_view">관리 그룹 보기</button>}
+    </aside>
+  );
+};
+
+export default SideBar;
