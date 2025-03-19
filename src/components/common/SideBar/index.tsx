e522b4c884854c93cba83ea249a9dc16c973b07e
yeowun05-12 <taking0n@naver.com>
2025-03-19 18:06:39 +0900

    🩹 [chore] 타입 수정

--- src/components/common/SideBar/index.tsx
+++ src/components/common/SideBar/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './Sidebar.scss';
 
 interface SidebarProps {
-  userType: 'guest' | 'user' | 'admin'; // 일반 사용자 or 관리자
+  userType?: 'guest' | 'user' | 'admin'; // 일반 사용자 or 관리자
 }
 
 const SideBar: React.FC<SidebarProps> = ({ userType }) => {
