f4d88df28f025d8fd15050cc128aaa06184bc90e
yeowun05-12 <taking0n@naver.com>
2025-03-21 00:59:37 +0900

    ✨[feat] 마이페이지 제작

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Join from './pages/Join.tsx';
 import Loginpage from './pages/Loginpage';
-import Mypage from './pages/Mypage.tsx';
+import Mypage from './pages/Mypage';
 import Main from './pages/Main.tsx';
 import Layout from './components/common/Layout.tsx';
 
@@ -10,10 +10,10 @@ function App() {
     <Routes>
       {/* 🔥 Layout을 적용할 부모 Route 추가 */}
       <Route element={<Layout />}>
-        <Route path="/" element={<Main />} />
-        <Route path="/join" element={<Join />} />
         <Route path="/mypage" element={<Mypage />} />
+        <Route path="/" element={<Main />} />
       </Route>
+      <Route path="/join" element={<Join />} />
       <Route path="/loginpage" element={<Loginpage />} />
     </Routes>
   );
---
+++ src/pages/Mypage/index.tsx
@@ -0,0 +1,149 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import right from '../../assets/chevron-right.png';
+import './mypage.scss';
+function Mypage() {
+  const [userRole, setUserRole] = useState('admin');
+  const navigate = useNavigate();
+  const Role = { USER: 'user', ADMIN: 'admin' };
+
+  useEffect(() => {
+    if (![Role.USER, Role.ADMIN].includes(userRole)) {
+      setUserRole(Role.USER); // 🚀 잘못된 값이면 기본값('user')으로 설정
+    }
+  }, [userRole]); // userRole이 변경될 때만 실행
+
+  return (
+    <>
+      {userRole === 'user' ? (
+        <div className="mypage_container">
+          <div className="mypage_title">
+            <h2>마이페이지</h2>
+            <p>원하시는 서비스를 선택해주세요</p>
+          </div>
+          <div className="user_info">
+            <div className="nickname-wrapper">
+              <h1 className="nickname">동해번쩍홍길동</h1>
+              <span>님</span>
+            </div>
+            <p className="user-email">3399__ss@naver.com</p>
+          </div>
+          <div className="mypage_list">
+            <ul>
+              <li
+                className="mypage_index"
+                onClick={() => {
+                  navigate('/user-info');
+                }}
+              >
+                <img src={right} alt="오른쪽 화살표 이미지" />
+                <p>회원정보 수정</p>
+              </li>
+              <li
+                className="mypage_index"
+                onClick={() => {
+                  navigate('/schedule-alarm');
+                }}
+              >
+                <img src={right} alt="오른쪽 화살표 이미지" />
+                <p>알림 설정</p>
+              </li>
+              <li
+                className="mypage_index"
+                onClick={() => {
+                  navigate('/user-info');
+                }}
+              >
+                <img src={right} alt="오른쪽 화살표 이미지" />
+                <p>개인정보 처리 방침</p>
+              </li>
+              <li
+                className="mypage_index"
+                onClick={() => {
+                  navigate('/withdrawal');
+                }}
+              >
+                <img src={right} alt="오른쪽 화살표 이미지" />
+                <p>회원 탈퇴</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      ) : (
+        <div className="mypage_container">
+          <div className="mypage_title">
+            <h2>마이페이지</h2>
+            <p>원하시는 서비스를 선택해주세요</p>
+          </div>
+          <div className="user_info">
+            <div className="nickname-wrapper">
+              <h1 className="nickname">동해번쩍홍길동</h1>
+              <span>님</span>
+            </div>
+            <p className="user-email">3399__ss@naver.com</p>
+          </div>
+          <div className="mypage_list">
+            <ul>
+              <li
+                className="mypage_index"
+                onClick={() => {
+                  navigate('/user-info');
+                }}
+              >
+                <img src={right} alt="오른쪽 화살표 이미지" />
+                <p>회원정보 수정</p>
+              </li>
+              <li
+                className="mypage_index"
+                onClick={() => {
+                  navigate('/group-add');
+                }}
+              >
+                <img src={right} alt="오른쪽 화살표 이미지" />
+                <p>그룹 추가</p>
+              </li>
+              <li
+                className="mypage_index"
+                onClick={() => {
+                  navigate('/group-list');
+                }}
+              >
+                <img src={right} alt="오른쪽 화살표 이미지" />
+                <p> 관리 그룹 보기 </p>
+              </li>
+              <li
+                className="mypage_index"
+                onClick={() => {
+                  navigate('/schedule-alarm');
+                }}
+              >
+                <img src={right} alt="오른쪽 화살표 이미지" />
+                <p>알림 설정</p>
+              </li>
+              <li
+                className="mypage_index"
+                onClick={() => {
+                  navigate('/user-info');
+                }}
+              >
+                <img src={right} alt="오른쪽 화살표 이미지" />
+                <p>개인정보 처리 방침</p>
+              </li>
+              <li
+                className="mypage_index"
+                onClick={() => {
+                  navigate('/withdrawal');
+                }}
+              >
+                <img src={right} alt="오른쪽 화살표 이미지" />
+                <p>회원 탈퇴</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Mypage;
