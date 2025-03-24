e8c4b9f8f83c0d6ed430d35dac65b69301834059
yeowun05-12 <taking0n@naver.com>
2025-03-25 03:17:00 +0900

    🎨[Design] 그룹 목록 수정 중

--- src/components/admin/Groupmanagement/index.tsx
+++ src/components/admin/Groupmanagement/index.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom'; // 페이지 이동을 위한 훅 추가
-import './GroupManagement.scss';
+import './Groupmanagement.scss';
 import instagramIcon from '../../../assets/instagram.png'; // 인스타그램 아이콘 추가
 import type { GroupType } from './GroupManagement.types';
 
