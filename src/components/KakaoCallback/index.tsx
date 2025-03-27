7b8e7710ed786e1eed866dc13db84a2641ce6675
yeowun05-12 <taking0n@naver.com>
2025-03-27 22:45:49 +0900

    🔧 백엔드 서버 주소 변경

--- src/components/KakaoCallback/index.tsx
+++ src/components/KakaoCallback/index.tsx
@@ -24,7 +24,7 @@ function KakaoCallBack() {
     console.log(code);
     try {
       const response = await axios.post(
-        'http://100.26.111.172/ilog/account/social-login/',
+        'https://ilog.giize.com/account/social-login/',
         {
           social_type: 'kakao',
           code: code,
