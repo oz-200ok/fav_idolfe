7813d4644fedc0bc328f4f9f1c9e754fcc9b585d
yeowun05-12 <taking0n@naver.com>
2025-04-01 01:10:32 +0900

    서버 주소 띄어쓰기 삭제...

--- src/utils/apiConfig.ts
+++ src/utils/apiConfig.ts
@@ -1,7 +1,7 @@
 // axios 설정을 담은 객체
 
 export const apiConfig = {
-  baseURL: 'https://ilog.giize.com/ilog/ ',
+  baseURL: 'https://ilog.giize.com/ilog/',
   headers: {
     'Content-Type': 'application/json',
   },
