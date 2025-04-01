6da27ecf0b2dd0d40921f505b0b8281f839a92b5
yeowun05-12 <taking0n@naver.com>
2025-04-01 23:07:25 +0900

    🔧 회원 탈퇴 및 로그아웃 확인 필요

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -27,12 +27,12 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     localStorage.setItem('refresh_token', refresh_token);
   };
 
-  const markLoggedOut = async () => {
+  const markLoggedOut = async (skipApi?: boolean ) => {
     const access_token = localStorage.getItem('access_token');
     const refresh_token = localStorage.getItem('refresh_token');
 
     try {
-      if (access_token && refresh_token) {
+      if (!skipApi && access_token && refresh_token) {
         await logout(access_token, refresh_token);
       }
     } catch (error) {
---
+++ src/pages/Joinpage/joinpage.ts
@@ -0,0 +1,8 @@
+export interface I_JoinFormValues {
+  email: string;
+  password: string;
+  passwordConfirm: string;
+  name: string;
+  username: string;
+  phone: string;
+}
