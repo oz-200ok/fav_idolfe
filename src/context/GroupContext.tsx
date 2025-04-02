9db4dc1d391ab8807f771b8ebc0816d87ca8a7cf
yeowun05-12 <taking0n@naver.com>
2025-04-02 19:44:18 +0900

    그룹 수정 중

--- src/context/GroupContext.tsx
+++ src/context/GroupContext.tsx
@@ -1,10 +1,4 @@
-import {
-  createContext,
-  useContext,
-  useState,
-  useEffect,
-  ReactNode,
-} from 'react';
+import { createContext, useContext, useState, ReactNode } from 'react';
 import { fetchGroupList } from '@/utils/group';
 import UserInstance from '@/utils/UserInstance';
 
