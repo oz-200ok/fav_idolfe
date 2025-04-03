54e2b93d72da9c7593e75d95aa3e7287673ad26c
00iun <lsh681236@naver.com>
2025-04-03 12:57:13 +0900

    reset

--- src/components/Schedule/Munsley/index.tsx
+++ src/components/Schedule/Munsley/index.tsx
@@ -7,29 +7,6 @@ import { T_use_Date, T_use_Modal, T_use_ScheduleType } from '../type';
 type T_Munsley_Props = T_use_Date & T_use_Modal & T_use_ScheduleType;
 
 export default function Munsley(props: T_Munsley_Props) {
-  // 일정 스케줄 ID 가 변경됨과 동시에 data를 일정값으로 변경
-
-  // function asdqwe(celendar: string, startTime: string) {
-  //   const adfa = {
-  //     Jan: '01',
-  //     Feb: '02',
-  //     Mar: '03',
-  //     Apr: '04',
-  //     May: '05',
-  //     Jun: '06',
-  //     Jul: '07',
-  //     Aug: '08',
-  //     Sep: '09',
-  //     Oct: '10',
-  //     Nov: '11',
-  //     Dec: '12',
-  //   };
-  //   const DATA_celendar = celendar.split(' ').slice(1, 4);
-  //   const DATA_time = startTime.slice(0, 10);
-  //   console.log(DATA_celendar);
-  //   console.log(DATA_time);
-  //   return;
-  // } // 나중에 유틸로 뺄 것!
   return (
     <div>
       <Calendar
@@ -46,19 +23,6 @@ export default function Munsley(props: T_Munsley_Props) {
           props.setSaveType('월');
           props.setScheduleType('일정');
           props.setDate(new Date(value));
-          // const timeText = '2025-03-15T 14:00:00+09:00'.slice(0, 10);
-          // const text = `${value}`;
-          // const number = text.split(' ').slice(1, 4);
-          // const draaqae = 'Mar';
-          // const month = { Mar: '03' };
-          // const drae = `${number[2]}-${month[draaqae]}-${number[1]}`;
-          // console.log(timeText, drae);
-          // console.log(value);
-          // asdqwe(`${value}`, '2025-03-15T 14:00:00+09:00')
-
-          // props로 받아온 일정을 돌려 동일한 날짜인지 대조
-          // 성공: setScheduleID(일정스케줄id) + setData({일정})
-          // 실패: setScheduleID(null)
         }}
         key={new Date().setMilliseconds(1)} // 리렌더링을 위한 임의의 key 입력 (값은 중복될 확률이 적은 값으로 지정한 것)
       />
